562f278b27b4a1e242b02b02cf34e9e7a4688cc6
shubham <khobragade.shubham@gmail.com>
2023-05-20 14:15:00 +0530

    added new functionalities

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,27 @@
 import React, { useRef, useState } from 'react'
+import { useEffect } from 'react';
 
 import { useNavigate, Link } from "react-router-dom";
 
 
 
 function Home() {
+
+   
+    const mainRef = useRef(null);
     const [columns, setColumns] = useState([]);
     const [currentPlayerDivs, setCurrentPlayerDivs] = useState([]);
     const [currentBankerDivs, setCurrentBankerDivs] = useState([]);
     const [divCounter, setDivCounter] = useState(0);
+    const [createdDivs, setCreatedDivs] = useState([]);
+    const [value, setValue] = useState('');
+    const [flag, setFlag] = useState(false);
 
+    
     const [input, setInput] = useState('');
     const [suggestion, setSuggestion] = useState('');
-
-
+    const [newSuggestion, setNewSuggestion] = useState('');
+    
     function createNewColumn() {
         const column = (
             <div key={columns.length} className="column">
@@ -27,43 +35,33 @@ function Home() {
         setDivCounter(0);
     }
 
+
+
     function createDiv(type) {
-        if (input.length > 3) {
-            var symbols = input.split("");
-            var length = symbols.length;
-            var answer = "";
-            if (symbols[length - 4] === symbols[length - 1]) {
-                answer = symbols[length - 4 + 1];
-            } else {
-                answer = symbols[length - 4 + 1] === "B" ? "P" : "B";
-            }
-            setSuggestion(answer);
-            console.log("ans ", answer);
-        } else {
-            setSuggestion("");
-        }
-        const className = type === 'player' ? 'pCapital' : 'bCapital';
+
         const value = type === 'player' ? 'P' : 'B';
+        setInput(prevInput => prevInput + value) 
 
-        setInput(prevInput => prevInput + value);
-
+        const className = type === 'player' ? 'pCapital' : 'bCapital';
+        const capitalValue = type === 'player' ? 'P' : 'B';
+        
         if (type === 'player') {
             if (currentBankerDivs.length > 0) {
                 createNewColumn();
             }
-            setCurrentPlayerDivs(prevDivs => [...prevDivs, createPlayerDiv(value, className)]);
+            const playerDiv = createPlayerDiv(capitalValue, className);
+            setCurrentPlayerDivs(prevDivs => [...prevDivs, playerDiv]);
+            setCreatedDivs(prevDivs => [...prevDivs, playerDiv]);
         } else if (type === 'banker') {
             if (currentPlayerDivs.length > 0) {
                 createNewColumn();
             }
-            setCurrentBankerDivs(prevDivs => [...prevDivs, createBankerDiv(value, className)]);
+            const bankerDiv = createBankerDiv(capitalValue, className);
+            setCurrentBankerDivs(prevDivs => [...prevDivs, bankerDiv]);
+            setCreatedDivs(prevDivs => [...prevDivs, bankerDiv]);
         }
-
-
-
-
     }
-
+    
     function createPlayerDiv(value, className) {
         return (
             <div key={divCounter} className={className}>
@@ -71,7 +69,7 @@ function Home() {
             </div>
         );
     }
-
+    
     function createBankerDiv(value, className) {
         return (
             <div key={divCounter} className={className}>
@@ -80,11 +78,168 @@ function Home() {
         );
     }
 
+    function undoDiv() { 
+        const lastDivIndex = createdDivs.length - 1;
+        console.log(lastDivIndex,"last div")
+        if (lastDivIndex >= 0) {
+            const lastDiv = createdDivs[lastDivIndex];
+            console.log(lastDiv,"last div",1)
+            if (lastDiv.props.className === 'pCapital') {
+                setCurrentPlayerDivs(prevDivs => prevDivs.slice(0, -1));
+                console.log(currentPlayerDivs,"last div",2)
+            } else if (lastDiv.props.className === 'bCapital') {
+                setCurrentBankerDivs(prevDivs => prevDivs.slice(0, -1));
+                console.log(currentPlayerDivs,"last div",2)
+            }
+            console.log(createdDivs,"last div",3)
+            setCreatedDivs(prevDivs => prevDivs.slice(0, -1));
+            console.log(createdDivs,"last div",3)
+
+            if (currentPlayerDivs.length === 0 && currentBankerDivs.length === 0 && columns.length > 0) {
+                console.log(columns,"last div",4)
+                const lastColumnIndex = columns.length - 1;
+                console.log(lastColumnIndex,"current column")
+                setColumns(prevColumns => prevColumns.slice(0, lastColumnIndex));
+                console.log(columns,"last div",5)
+            }
+        }
+    
+    }
+
+
+    // function undoDiv() {
+    //     const lastDivIndex = createdDivs.length - 1;
+    //     if (lastDivIndex >= 0) {
+    //       const lastDiv = createdDivs[lastDivIndex];
+    //       if (lastDiv.props.className === 'pCapital') {
+    //         setCurrentPlayerDivs(prevDivs => prevDivs.slice(0, -1));
+    //       } else if (lastDiv.props.className === 'bCapital') {
+    //         setCurrentBankerDivs(prevDivs => prevDivs.slice(0, -1));
+    //       }
+    //       setCreatedDivs(prevDivs => prevDivs.slice(0, -1));
+      
+    //       if (currentPlayerDivs.length === 0 && currentBankerDivs.length === 0 && columns.length > 0) {
+    //         const lastColumnIndex = columns.length - 1;
+    //         const previousColumnIndex = lastColumnIndex - 1;
+      
+    //         const updatedColumns = [...columns];
+      
+    //         if (previousColumnIndex >= 0) {
+    //           const previousColumn = updatedColumns[previousColumnIndex];
+    //           const updatedPreviousColumn = React.cloneElement(previousColumn, {
+    //             children: removeLastDivFromColumn(previousColumn.props.children)
+    //           });
+    //           updatedColumns.splice(previousColumnIndex, 1, updatedPreviousColumn);
+    //         }
+      
+    //         setColumns(updatedColumns);
+    //       }
+    //     }
+    //   }
+      
+    //   function removeLastDivFromColumn(columnChildren) {
+    //     const reversedChildren = React.Children.toArray(columnChildren).reverse();
+    //     let updatedChildren = [...reversedChildren];
+    //     let found = false;
+      
+    //     for (let i = 0; i < updatedChildren.length; i++) {
+    //       const child = updatedChildren[i];
+    //       if (child.props.className === 'pCapital' || child.props.className === 'bCapital') {
+    //         updatedChildren.splice(i, 1);
+    //         found = true;
+    //         break;
+    //       }
+    //     }
+      
+    //     if (found) {
+    //       updatedChildren = updatedChildren.reverse();
+    //     }
+      
+    //     return updatedChildren;
+    //   }
+      
+      
+      
+
+
+
+    // var lastSymbol = symbols[symbols.length - 1];
+
+      
+      
+
+        
+    // useEffect(() => {
+    //     console.log(input,"PBPB")
+    //     if (input.length > 3) {
+    //       var symbols = input.split("");
+    //       var length = symbols.length;
+    //       var answer = "";
+  
+    //       if (symbols[length - 4] === symbols[length - 1]) {
+    //         answer = symbols[length - 4 + 1];
+    //       } else {
+    //         answer = symbols[length - 4 + 1] === "B" ? "P" : "B";
+    //       }
+    //       setSuggestion(answer);
+    //       setNewSuggestion(answer);
+    //       console.log("ans ", answer);
+    //     } else {
+    //       setSuggestion("jfasdjl");
+    //     }
+    //   }, [input]);
 
 
 
 
 
+
+    //   useEffect(() => {
+    //     console.log(newSuggestion,input,"suggestion")
+    //     // if(suggestion !===  )
+ 
+    //   }, [newSuggestion])
+      
+
+    
+
+
+        useEffect(() => {
+            
+          
+            // console.log(answer,"dssdfafd",input)
+            // if(answer != input.split("")[input.length-1]){
+            //     console.log("dssdfafd")
+            // }
+          
+        console.log(input,"PBPB")
+        if (input.length > 3) {
+            console.log(input,"previous input")
+          var symbols = input.split("");
+          var length = symbols.length;
+          var answer = "";
+          
+          if (symbols[length - 4] === symbols[length - 1]) {
+            answer = symbols[length - 4 + 1];
+          } else {
+            answer = symbols[length - 4 + 1] === "B" ? "P" : "B";
+          }
+
+          console.log(answer,"answer")
+        if(answer != symbols[length -1]){
+                console.log("dssdfafd","answer")
+            }
+
+          setSuggestion(answer);
+          setNewSuggestion(answer);
+          console.log("ans ", answer);
+        }else {
+                  setSuggestion("jfasdjl");
+               } 
+      }, [input]);
+
+    
+    
 
     return (
         <>
@@ -171,7 +326,7 @@ function Home() {
                             <div className="second-div">17</div>
                         </div>
                     </div>
-                    <div className="columnsContainer">
+                    <div ref={mainRef} className="columnsContainer">
                         {columns}
                         <div className="column">
                             {currentPlayerDivs}
@@ -186,14 +341,22 @@ function Home() {
                     </div>
 
                     <div className="bottom-row">
-                        <button id="playerBtn" onClick={() => createDiv('player')} style={{ backgroundColor: "#006ACC", color: "white", fontSize: "0.8rem" }} className="bottom-button">Player</button>
-                        <button id="bankerBtn" onClick={() => createDiv('banker')} style={{ backgroundColor: "#CC0000", color: "white", fontSize: "0.8rem" }} className="bottom-button">Banker</button>
+                        {/* <button id="playerBtn" onClick={() => createDiv('player')} style={{ backgroundColor: "#006ACC", color: "white", fontSize: "0.8rem" }} className="bottom-button">Player</button>
+                            <button id="bankerBtn" onClick={() => createDiv('banker')} style={{ backgroundColor: "#CC0000", color: "white", fontSize: "0.8rem" }} className="bottom-button">Banker</button> */}
+                        <button id="playerBtn" onClick={() => { setValue('Player Value'); createDiv('player'); }} style={{ backgroundColor: "#006ACC", color: "white", fontSize: "0.8rem" }} className="bottom-button">Player</button>
+                        
+    <button id="bankerBtn" onClick={() => { setValue('Banker Value'); createDiv('banker'); }} style={{ backgroundColor: "#CC0000", color: "white", fontSize: "0.8rem" }} className="bottom-button">Banker</button>
                         <button className="bottom-button">1000</button>
                         <button className="bottom-button">1000</button>
 
                     </div>
+                    <div className="undo-div">
+
+                    </div>
                 </div>
             </div>
+            <button className='suggestion' onClick={undoDiv}>Undo</button>
+
 
         </>
     )
